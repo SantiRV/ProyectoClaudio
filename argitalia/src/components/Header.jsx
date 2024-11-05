67c966a073680bee668e760ca29cbff9b570415b
Santiago Rambeaud Vazquez <santirambeaudvazquez@gmail.com>
2024-11-05 15:16:55 +0100

    Diseño de Header implementado

--- argitalia/src/components/Header.jsx
+++ argitalia/src/components/Header.jsx
@@ -1,26 +1,26 @@
-// src/components/Header.js
+import "../styles/Header.css";
 import React from 'react';
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand">Argitalia Ciudadanias</a>
+        <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#ca6f1e' }}>
+            <a className="navbar-brand ml-3 text-white">Argitalia Ciudadanias</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#quienes-somos">Quiénes Somos?</a>
+                    <li className="nav-item mx-3">
+                        <a className="nav-link text-white" href="#quienes-somos">Quiénes Somos?</a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#que-hacemos">Qué Hacemos?</a>
+                    <li className="nav-item mx-3">
+                        <a className="nav-link text-white" href="#que-hacemos">Qué Hacemos?</a>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#donde-estamos">Dónde Estamos?</a>
+                    <li className="nav-item mx-3">
+                        <a className="nav-link text-white" href="#donde-estamos">Dónde Estamos?</a>
                     </li>
-                    <li>
-                        <a className='nav-link' href='#Dejanos-tu-mensaje'>Dejanos tu mensaje</a>
+                    <li className="nav-item mx-3">
+                        <a className='nav-link text-white' href='#Dejanos-tu-mensaje'>Dejanos tu mensaje</a>
                     </li>
                 </ul>
             </div>
@@ -29,5 +29,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
